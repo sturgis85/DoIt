dbcd03cfb27d71f31d87a50d76a7084cee11be2a
John Fiedler <doscattos@gmail.com>
2016-11-01 18:07:35 -0400

    End of lecture 57

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,17 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()     // we're creating a tableviewcell object in cell
+        cell.textLabel?.text = "Hello"   // and for now, we'll just show we can manipulate it
+        return cell
+    }
+    
 
 
 }

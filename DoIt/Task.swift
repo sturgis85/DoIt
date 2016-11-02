2945dcd20e3416636989bc98abb3fb5e4d81da06
John Fiedler <doscattos@gmail.com>
2016-11-02 18:37:56 -0400

    After lecture 58

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by John Fiedler on 11/2/16.
+//  Copyright © 2016 SlobberHound. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet var tableView: UITableView!
     
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tasks = makeTasks()
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -25,15 +28,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()     // we're creating a tableviewcell object in cell
-        cell.textLabel?.text = "Hello"   // and for now, we'll just show we can manipulate it
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "! \(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
+
         return cell
     }
     
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+
+        let task2 = Task()
+        task2.name = "Buy cheese"
+        task2.important = true
+
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
+    }
 
 
 }
